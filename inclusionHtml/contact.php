879b1dd9f1fbf6957af75173b694f95b4f314dfe
davK91 <david.kalala91@gmail.com>
2023-03-02 09:23:40 +0100

    Mise en forme

--- inclusionHtml/contact.php
+++ inclusionHtml/contact.php
@@ -6,24 +6,38 @@
 ?>
 
 <form action="process_contact.php" method="post">
-    <label for="firstname">Prénom :</label>
+    <label for="firstname">Prénom :</label><br>
     <input type="text" id="firstname" name="firstname" required> <br>
 
-    <label for="lastname">Nom :</label>
+    <label for="lastname">Nom :</label> <br>
     <input type="text" id="lastname" name="lastname" required> <br>
 
-    <label for="email">Email :</label>
+    <label for="email">Email :</label><br>
     <input type="email" id="email" name="email" required> <br>
 
 
-    <label for="subject">Sujet :</label>
-    <input type="text" id="subject" name="subject" required>
+    <label for="subject">Sujet :</label><br>
+    <input type="text" id="subject" name="subject" required> <br>
 
-    <label for="message">Message :</label>
-    <textarea id="message" name="message" required></textarea>
+    <label for="message">Message :</label><br>
+    <textarea id="message" name="message" required></textarea> <br>
 
     <button type="submit" name="submit">Envoyer</button>
 </form>
+
+<?php
+    if (isset($_POST['submit'])) {
+    // Le formulaire a été soumis, traiter les données ici
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+    // Faire quelque chose avec les données (envoyer un email, les enregistrer dans une base de données, etc.)
+    }
+?>
+
 
 <?php 
 
